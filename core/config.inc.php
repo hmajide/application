805dd86b9dd094c5e75247f25517dbefddb91ddb
Hmajide <hadhoud.majide@gmail.com>
2018-12-14 11:06:41 +0100

    Yes commit

---
+++ core/config.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hmajide
+ * Date: 13/12/2018
+ * Time: 14:30
+ */
+
+/**
+ * Database
+ */
+
+ define('DB_TYPE', 'mysql');
+ define('DB_HOST', 'localhost');
+ define('DB_NAME', 'Collectify');
+ define('DB_USER', 'root');
+ define('DB_PASSWORD', '');
+
+
+/**
+ * Redbean configuration
+ */
+
+define('REDBEAN_MODEL_PREFIX', '\\Collectify\\Model');
+
+
+
+?>
